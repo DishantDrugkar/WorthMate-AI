c4f900bc82e6757f68add53db9094565808a0783
DishantDrugkar <dishantdrugkar4@gmail.com>
2026-02-11 18:45:42 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/worthmate/demo/WorthmateApplication.java
@@ -0,0 +1,13 @@
+package com.worthmate.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorthmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorthmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worthmate/demo/WorthmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worthmate.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorthmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
